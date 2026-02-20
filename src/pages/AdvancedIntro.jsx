91a5c29b1192ee6b7ccacf6fe67a6ffb63cae626
Eneagrama App <admin@eneagrama.app>
2026-02-20 14:59:13 -0500

    Fix: Broken logo image on /test-liderazgo by using standard brand logo

--- src/pages/AdvancedIntro.jsx
+++ src/pages/AdvancedIntro.jsx
@@ -80,7 +80,7 @@ const AdvancedIntro = ({ onRegister, user: existingUser, targetRoute = '/advance
                 <div className="adv-content-wrapper">
                     <div className="home-logo-container">
                         <img
-                            src="/moneda-autenticos.png"
+                            src="/Circulo_Eneagrama_Autenticos_02.png"
                             alt="Logo Eneagrama - Autenticos"
                             className="home-logo animate-zoom-in-slow"
                         />
